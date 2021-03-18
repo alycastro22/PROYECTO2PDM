fef5315db8c7c593b37772c567d956b336f632bd
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-03-17 19:27:59 -0600

    Arreglo de fragment menu

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/EmpleadosFragment.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/EmpleadosFragment.kt
@@ -34,7 +34,7 @@ class EmpleadosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empleados, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
     companion object {
--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/MenuFragment.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/MenuFragment.kt
@@ -34,7 +34,7 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(R.layout.fragment_empleados, container, false)
     }
 
     companion object {
