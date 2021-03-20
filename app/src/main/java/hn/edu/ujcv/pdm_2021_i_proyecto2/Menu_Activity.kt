f1f60b6de2d2158f9dc6f82096913c1be30719ce
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-03-19 20:37:33 -0600

    Arreglo de fragment menu y clientes a empty activity.

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/Menu_Activity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_)
+    }
+}
