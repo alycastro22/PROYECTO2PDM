7edb05d873d6e0a21272e8f25d900bbaf25ff516
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-03-20 20:37:38 -0600

    Pantalla factura

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/Diseno_Factura_Activity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Diseno_Factura_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diseno_factura)
+    }
+}
