e7116bcaafe3c07a7d2972f208883ae9c2bd2930
nahomi <nahomi.martinez@ujcv.edu.hn>
2021-03-22 01:00:35 -0600

    Holis :)

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/Pedido_Activity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_proyecto2/Pedido_Activity.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_empleados_.*
 import kotlinx.android.synthetic.main.activity_pedido_.*
+import kotlinx.android.synthetic.main.activity_pedido_.imageRetroceder
+import kotlinx.android.synthetic.main.activity_pedido_.spinnerClientes
+import java.lang.StringBuilder
 
 class Pedido_Activity : AppCompatActivity() {
     var clientes:ArrayList<String> = ArrayList()
@@ -21,6 +26,8 @@ class Pedido_Activity : AppCompatActivity() {
         datos()
         imageRetroceder.setOnClickListener { cambioPantalla() }
         agregarItemsClientes()
+        agregarItemsEmpleado()
+        agregarItemsMenu()
        // iniciabilizador()
     }
 
@@ -31,9 +38,32 @@ class Pedido_Activity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
         spinner.adapter = adapter
     }
-    fun guardarpedido(){
-
+    fun agregarItemsEmpleado(){
+        val spinner : Spinner = findViewById(R.id.spinnerEmpleado1)
+        val adapter : ArrayAdapter<String> = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, empleados)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinner.adapter = adapter
     }
+    fun agregarItemsMenu(){
+        val spinner : Spinner = findViewById(R.id.spinnerVariosMenus)
+        val adapter : ArrayAdapter<String> = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, menus)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinner.adapter = adapter
+    }
+    fun guardarpedido(){
+        if(txtCodigo.text.isNotEmpty() && txtNombre3.text.isNotEmpty()){
+            val dato = StringBuilder()
+            dato.append(spinnerClientes.selectedItem.toString().trim()).append("|")
+            dato.append(spinnerVariosMenus.selectedItem.toString().trim()).append("|")
+            dato.append(spinnerEmpleado1.selectedItem.toString().trim())
+            pedido.add(dato.toString())
+            println(pedido.toString() + "Datos de pedido")
+            Toast.makeText(this, "Pedido registrado con exito", Toast.LENGTH_LONG).show()
+        }
+    }
+
     fun cambioPantalla() {
         valor=5
         val cambio = Intent(this, MainActivity::class.java)
